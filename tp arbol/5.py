82de689ea105b15c873a33bd1b17d92e5ed7b8b3
soperez03 <163930556+soperez03@users.noreply.github.com>
2024-11-10 13:44:14 -0300

    Create 5.py

---
+++ tp arbol/5.py
@@ -0,0 +1,396 @@
+'''
+5. Dado un árbol con los nombre de los superhéroes y villanos de la saga Marvel Cinematic Univer-
+se (MCU), desarrollar un algoritmo que contemple lo siguiente:
+
+a. además del nombre del superhéroe, en cada nodo del árbol se almacenará un campo boo-
+leano que indica si es un héroe o un villano, True y False respectivamente;
+
+b. listar los villanos ordenados alfabéticamente;
+c. mostrar todos los superhéroes que empiezan con C;
+d. determinar cuántos superhéroes hay el árbol;
+e. Doctor Strange en realidad está mal cargado. Utilice una búsqueda por proximidad para
+encontrarlo en el árbol y modificar su nombre;
+f. listar los superhéroes ordenados de manera descendente;
+g. generar un bosque a partir de este árbol, un árbol debe contener a los superhéroes y otro a
+los villanos, luego resolver las siguiente tareas:
+I. determinar cuántos nodos tiene cada árbol;
+II. realizar un barrido ordenado alfabéticamente de cada árbol.'''
+from arbol_avl import BinaryTree
+
+class BinaryTree:
+
+    class __Node:
+        def __init__(self, value, left=None, right=None, other_value=None):
+            self.value = value
+            self.left = left
+            self.right = right
+            self.other_value = other_value
+
+    def __init__(self):
+        self.root = None
+
+    def insert_node(self, value, other_value=None):
+        def __insert(root, value, other_value=None):
+            if root is None:
+                return BinaryTree.__Node(value, other_value=other_value)
+            elif value < root.value:
+                root.left = __insert(root.left, value, other_value)
+            else:
+                root.right = __insert(root.right, value, other_value)
+            return root
+
+        self.root = __insert(self.root, value, other_value)
+
+    def search(self, key):
+        def __search(root, key):
+            if root is not None:
+                if root.value == key:
+                    # print('lo encontre')
+                    return root
+                elif key < root.value:
+                    # print(f'buscalo a la izquierda de {root.value}')
+                    return __search(root.left, key)
+                else:
+                    # print(f'buscalo a la derecha de {root.value}')
+                    return __search(root.right, key)
+            # else:
+            #     print('no hay nada')
+        aux = None
+        if self.root is not None:
+            aux = __search(self.root, key)
+        return aux
+
+    def preorden(self):
+        def __preorden(root):
+            if root is not None:
+                print(root.value)
+                # print(f'izquierda de {root.value}')
+                __preorden(root.left)
+                # print(f'derecha de {root.value}')
+                __preorden(root.right)
+
+        if self.root is not None:
+            __preorden(self.root)
+
+    def contar_super_heroes(self):
+        def __contar_super_heroes(root):
+            counter = 0
+            if root is not None:
+                if root.other_value.get('is_hero') is True:
+                    counter = 1
+                counter += __contar_super_heroes(root.left)
+                counter += __contar_super_heroes(root.right)
+            return counter
+
+        return __contar_super_heroes(self.root)
+    def inorden(self):
+      def __inorden(root):
+        if root is not None:
+          __inorden(root.left)
+          print(root.value)
+          __inorden(root.right)
+
+      if self.root is not None:
+        __inorden(self.root)
+
+    
+    def postorden(self):
+        def __postorden(root):
+            if root is not None:
+                __postorden(root.right)
+                print(root.value)
+                __postorden(root.left)
+
+        if self.root is not None:
+            __postorden(self.root)
+
+    def inorden_villanos(self):
+        def __inorden_villanos(root):
+            if root is not None:
+                __inorden_villanos(root.left)
+                if root.other_value.get('is_hero') is not True:
+                    print(root.value)
+                __inorden_villanos(root.right)
+
+        if self.root is not None:
+            __inorden_villanos(self.root)
+
+    def inorden_superheros_start_with(self, start):
+        def __inorden_superheros_start_with(root, start):
+            if root is not None:
+                __inorden_superheros_start_with(root.left, start)
+                if root.other_value.get('is_hero') is True and root.value.startswith(start):
+                    print(root.value)
+                __inorden_superheros_start_with(root.right, start)
+
+        if self.root is not None:
+            __inorden_superheros_start_with(self.root, start)
+
+    def by_level(self):
+        pendientes = Queue()
+        if self.root is not None:
+            pendientes.arrive(self.root)
+
+        while pendientes.size() > 0:
+            node = pendientes.attention()
+            print(node.value)
+            if node.left is not None:
+                pendientes.arrive(node.left)
+            if node.right is not None:
+                pendientes.arrive(node.right)
+
+    def delete_node(self, value):
+        def __replace(root):
+            if root.right is None:
+                # print(f'no tiene derecha es el mayor {root.value}')
+                return root.left, root
+            else:
+                # print('seguir buscando nodo par remplaz+ar a la dercha')
+                root.right, replace_node = __replace(root.right)
+                return root, replace_node
+
+        def __delete(root, value):
+            value_delete = None
+            if root is not None:
+                if root.value > value:
+                    # print(f'buscar  a la izquierda de {root.value}')
+                    root.left, value_delete = __delete(root.left, value)
+                elif root.value < value:
+                    # print(f'buscar  a la derecha de {root.value}')
+                    root.right, value_delete = __delete(root.right, value)
+                else:
+                    # print('valor encontrado')
+                    value_delete = root.value
+                    if root.left is None:
+                        # print('a la izquierda no hay nada')
+                        return root.right, value_delete
+                    elif root.right is None:
+                        # print('a la derecha  no hay nada')
+                        return root.left, value_delete
+                    else:
+                        # print('tiene ambos hijos')
+                        root.left, replace_node = __replace(root.left)
+                        root.value = replace_node.value
+                        return root, value_delete
+                
+            return root, value_delete
+
+        delete_value = None
+        if self.root is not None:
+            self.root, delete_value = __delete(self.root, value)
+        return 
+    def contar_villanos(self):
+        def __contar_villanos(root):
+            counter = 0
+            if root is not None:
+                if root.other_value.get('is_hero') is False:
+                    counter = 1
+                counter += __contar_villanos(root.left)
+                counter += __contar_villanos(root.right)
+            return counter
+
+        return __contar_villanos(self.root)
+
+super_heroes = [
+  {
+    "nombre": "Linterna Verde",
+    "año_aparicion": 1940,
+    "casa_comic": "DC Comics",
+    "biografia": "Miembro de la Tropa de Linternas Verdes, posee un anillo que le otorga poderes basados en la fuerza de voluntad."
+  },
+  {
+    "nombre": "Wolverine",
+    "año_aparicion": 1974,
+    "casa_comic": "Marvel Comics",
+    "biografia": "Mutante con garras retráctiles y habilidades regenerativas, miembro de los X-Men."
+  },
+  {
+    "nombre": "Dortor Strange",
+    "año_aparicion": 1963,
+    "casa_comic": "Marvel Comics",
+    "biografia": "Hechicero supremo del universo Marvel, maestro de las artes místicas y protector de la realidad."
+  },
+  {
+    "nombre": "Capitana Marvel",
+    "año_aparicion": 1968,
+    "casa_comic": "Marvel Comics",
+    "biografia": "Heroína cósmica con poderes de vuelo, fuerza sobrehumana y energía cósmica."
+  },
+  {
+    "nombre": "Mujer Maravilla",
+    "año_aparicion": 1941,
+    "casa_comic": "DC Comics",
+    "biografia": "Princesa amazona y una de las principales defensoras de la justicia y la igualdad en el Universo DC."
+  },
+  {
+    "nombre": "Flash",
+    "año_aparicion": 1940,
+    "casa_comic": "DC Comics",
+    "biografia": "Velocista con la capacidad de correr a velocidades superiores a la luz, miembro de la Liga de la Justicia."
+  },
+  {
+    "nombre": "Star-Lord",
+    "año_aparicion": 1976,
+    "casa_comic": "Marvel Comics",
+    "biografia": "Líder de los Guardianes de la Galaxia, experto en combate y estrategia intergaláctica."
+  },
+  {
+    "nombre": "Superman",
+    "año_aparicion": 1938,
+    "casa_comic": "DC Comics",
+    "biografia": "El Hombre de Acero, uno de los héroes más icónicos de DC con superpoderes sobrehumanos."
+  },
+  {
+    "nombre": "Batman",
+    "año_aparicion": 1939,
+    "casa_comic": "DC Comics",
+    "biografia": "El Caballero Oscuro, detective y luchador experto que protege Gotham City."
+  },
+  {
+    "nombre": "Iron Man",
+    "año_aparicion": 1963,
+    "casa_comic": "Marvel Comics",
+    "biografia": "Tony Stark, genio multimillonario y superhéroe con una armadura tecnológica de alta tecnología."
+  },
+  {
+    "nombre": "Wonder Woman",
+    "año_aparicion": 1941,
+    "casa_comic": "DC Comics",
+    "biografia": "La princesa amazona Diana, guerrera y defensora de la paz y la justicia en el mundo."
+  },
+  {
+    "nombre": "Spider-Man",
+    "año_aparicion": 1962,
+    "casa_comic": "Marvel Comics",
+    "biografia": "Peter Parker, joven héroe con habilidades arácnidas tras ser picado por una araña radiactiva."
+  },
+  {
+    "nombre": "Thor",
+    "año_aparicion": 1962,
+    "casa_comic": "Marvel Comics",
+    "biografia": "Dios nórdico del trueno y miembro de los Vengadores, posee un martillo encantado llamado Mjolnir."
+  },
+  {
+    "nombre": "Aquaman",
+    "año_aparicion": 1941,
+    "casa_comic": "DC Comics",
+    "biografia": "Rey de Atlantis con la capacidad de comunicarse con la vida marina y controlar el agua."
+  },
+  {
+    "nombre": "Green Arrow",
+    'villano': True,
+    "año_aparicion": 1941,
+    "casa_comic": "DC Comics",
+    "biografia": "Oliver Queen, arquero habilidoso y defensor de la justicia con su arco y flechas."
+  },
+  {
+    "nombre": "Hulk",
+    "año_aparicion": 1962,
+    "casa_comic": "Marvel Comics",
+    "biografia": "Bruce Banner, científico transformado en monstruo verde con fuerza increíble."
+  },
+  {
+    "nombre": "Black Widow",
+    "año_aparicion": 1964,
+    "casa_comic": "Marvel Comics",
+    "biografia": "Natasha Romanoff, espía rusa y experta en combate mano a mano y armas."
+  },
+  {
+    "nombre": "Mr. Fantástico",
+    "año_aparicion": 1961,
+    "casa_comic": "Marvel Comics",
+    "biografia": "Líder de los 4 Fantásticos, científico brillante con la capacidad de estirar y deformar su cuerpo."
+  },
+  {
+    "nombre": "La Mujer Invisible",
+    "año_aparicion": 1961,
+    "casa_comic": "Marvel Comics",
+    "biografia": "Miembro de los 4 Fantásticos, posee el poder de hacerse invisible y crear campos de fuerza."
+  },
+  {
+    "nombre": "La Alforcha Humana",
+    "año_aparicion": 1961,
+    "casa_comic": "Marvel Comics",
+    "biografia": "Miembro de los 4 Fantásticos, puede envolverse en llamas y volar a altas velocidades."
+  },
+  {
+    "nombre": "La Cosa",
+    "año_aparicion": 1961,
+    "casa_comic": "Marvel Comics",
+    "biografia": "Miembro de los 4 Fantásticos, posee una fuerza y resistencia sobrehumanas, con piel rocosa."
+  },
+  {
+    "nombre": "Capitán América",
+    "año_aparicion": 1941,
+    "casa_comic": "Marvel Comics",
+    "biografia": "El supersoldado Steve Rogers, símbolo de libertad y justicia con escudo indestructible."
+  },
+  {
+    "nombre": "Thanos",
+    'villano': True
+  },
+  {
+    "nombre": "Loctor Doom",
+    'villano': True
+  },
+  {
+    "nombre": "Green Golbing",
+    'villano': True
+  }
+]
+
+tree= BinaryTree()
+'''
+for personaje in super_heroes:
+    is_hero = False if 'villano' in personaje else True
+    personaje['is_hero']= is_hero
+    tree.insert_node(personaje['nombre'], personaje)
+
+#tree.inorden_villanos()
+
+c = tree.inorden_superheros_start_with('C')
+
+
+contador= tree.contar_super_heroes()
+print(f'la cantidad de super heroes es:{contador}')
+
+
+#tree.proximity_search('Dortor Strange')
+pos = tree.search('Dortor Strange')
+pos.value = 'Doctor Strange'
+print(pos)
+
+
+def puntof(self):
+  def __puntof(root): 
+    if root is not None:
+      puntof(root.right) 
+      print(root.value)      
+      puntof(root.left)   
+      puntof(self.root)
+tree.puntof()  '''
+
+
+a_superh= BinaryTree()
+a_villanos= BinaryTree()
+for personaje in super_heroes:
+  is_hero = False if 'villano' in personaje else True
+  personaje['is_hero']= is_hero
+  if is_hero:
+        a_superh.insert_node(personaje['nombre'], personaje)
+  else:
+        a_villanos.insert_node(personaje['nombre'], personaje)
+print('el arbol de super heroes es: ')
+a_superh.inorden()
+print()
+nodos_superheroes = a_superh.contar_super_heroes
+print('y la cantidad de nodos es: ', nodos_superheroes)
+
+
+print()
+print('el arbol vilanos es: ')
+a_villanos.inorden_villanos()
+nodos_villanos= a_villanos.contar_villanos()
+print('y la cantidad de nodos es: ', nodos_villanos)
+
+'''el recorrido inorden ya lo devuelve en orden alfabetico'''
